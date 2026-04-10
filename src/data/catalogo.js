1b4869d20d72ef88e94db52dfba38dbfac68b40d
dumanginlucas <dumanginlucas@gmail.com>
2026-04-10 13:05:12 -0300

    Add files via upload

--- src/data/catalogo.js
+++ src/data/catalogo.js
@@ -1,1 +1,20 @@
-// catálogo atualizado com novos ABV/IBU e produtos
+export const produtos = [
+{
+nome: "Hockenheim Pilsen Puro Malte",
+abv: "4.8%",
+ibu: "17",
+imagem: "/catalogo/pilsen-puro-malte.png"
+},
+{
+nome: "Hockenheim American IPA Rota 77",
+abv: "7.4%",
+ibu: "73",
+descricao: "Apresenta aroma e sabor bem-marcados pelos fortes lúpulos americanos, que nos trazem uma percepção sensorial de florais, cítricas, de pinho, frutas tropicais e vermelhas. Seca, refrescante e de corpo leve, uma American IPA de percepção de álcool suave e leve aquecimento.",
+imagem: "/catalogo/rota77.png"
+},
+{
+nome: "Hockenheim Pilsen",
+abv: "4.5%",
+ibu: "9"
+}
+];
